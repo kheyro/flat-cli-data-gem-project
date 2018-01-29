9dbc0068901b02e34421cc17921511882e56ee85
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-29 18:42:52 +0000

    first commit

---
+++ lib/flat/cli/data/gem/project.rb
@@ -0,0 +1,13 @@
+require "flat/cli/data/gem/project/version"
+
+module Flat
+  module Cli
+    module Data
+      module Gem
+        module Project
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ lib/flat/cli/data/gem/project/version.rb
@@ -0,0 +1,11 @@
+module Flat
+  module Cli
+    module Data
+      module Gem
+        module Project
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
